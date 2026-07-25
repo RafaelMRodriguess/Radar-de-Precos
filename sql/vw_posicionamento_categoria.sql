e7f431e5dbc0bd41402a580c085d4bdafc04c804
Rafael Machado Rodrigues <rafaelmachadorodrigues8@gmail.com>
2026-07-25 16:55:42 -0300

    Adiciona views de ranking para compor graficos

---
+++ sql/vw_posicionamento_categoria.sql
@@ -0,0 +1,18 @@
+WITH preco_produto AS(
+    SELECT
+        products.product_name AS nome_produto,
+        products.category AS categoria,
+        price_history.price AS preco
+    FROM products
+    JOIN price_history
+        ON price_history.product_id = products.product_id
+),
+ranking AS (
+    SELECT *,
+        ROW_NUMBER()OVER(PARTITION BY categoria ORDER BY preco DESC) AS ranking
+    FROM preco_produto
+)
+SELECT *
+FROM ranking
+WHERE ranking = 1
+ORDER BY categoria;
