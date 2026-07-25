bef17ddebe5b5684197e79d03645340ae29256b2
Rafael Machado Rodrigues <rafaelmachadorodrigues8@gmail.com>
2026-07-25 17:12:11 -0300

    Alteração nas views

--- sql/vw_ranking_produtos.sql
+++ sql/vw_ranking_produtos.sql
@@ -1,16 +1,17 @@
-WITH preco_produto AS(
-    SELECT
-        products.product_name AS nome_produto,
-        price_history.price AS preco
-    FROM products
-    JOIN price_history
-        ON products.product_id = price_history.product_id
-),
-ranking AS(
-    SELECT *,
-        ROW_NUMBER()OVER(ORDER BY preco DESC) AS ranking
-    FROM preco_produto
-)
-SELECT *
-FROM ranking
-WHERE ranking <= 10;
+CREATE OR REPLACE VIEW vw_ranking_produtos AS
+    WITH preco_produto AS(
+        SELECT
+            products.product_name AS nome_produto,
+            price_history.price AS preco
+        FROM products
+        JOIN price_history
+            ON products.product_id = price_history.product_id
+    ),
+    ranking AS(
+        SELECT *,
+            ROW_NUMBER()OVER(ORDER BY preco DESC) AS ranking
+        FROM preco_produto
+    )
+    SELECT *
+    FROM ranking
+    WHERE ranking <= 10;
