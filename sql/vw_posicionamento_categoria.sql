a671f6fd78cfc63246a63fa2f242ed1df9b58e50
Rafael Machado Rodrigues <rafaelmachadorodrigues8@gmail.com>
2026-07-25 19:42:15 -0300

    Traduz as categorias para PT-BR

--- sql/vw_posicionamento_categoria.sql
+++ sql/vw_posicionamento_categoria.sql
@@ -2,7 +2,33 @@ CREATE OR REPLACE VIEW vw_posicionamento_categoria AS
     WITH preco_produto AS(
         SELECT
             products.product_name AS nome_produto,
-            products.category AS categoria,
+            CASE
+                WHEN category = 'beauty' THEN 'Beleza'
+                WHEN category = 'fragrances' THEN 'Perfumes'
+                WHEN category = 'furniture' THEN 'Móveis'
+                WHEN category = 'groceries' THEN 'Mercearia'
+                WHEN category = 'home-decoration' THEN 'Decoração'
+                WHEN category = 'kitchen-accessories' THEN 'Acessórios de Cozinha'
+                WHEN category = 'laptops' THEN 'Notebooks'
+                WHEN category = 'mens-shirts' THEN 'Camisas Masculinas'
+                WHEN category = 'mens-shoes' THEN 'Calçados Masculinos'
+                WHEN category = 'mens-watches' THEN 'Relógios Masculinos'
+                WHEN category = 'mobile-accessories' THEN 'Acessórios para Celular'
+                WHEN category = 'motorcycle' THEN 'Motocicletas'
+                WHEN category = 'skin-care' THEN 'Cuidados com a Pele'
+                WHEN category = 'smartphones' THEN 'Smartphones'
+                WHEN category = 'sports-accessories' THEN 'Acessórios Esportivos'
+                WHEN category = 'sunglasses' THEN 'Óculos de Sol'
+                WHEN category = 'tablets' THEN 'Tablets'
+                WHEN category = 'tops' THEN 'Blusas'
+                WHEN category = 'vehicle' THEN 'Veículos'
+                WHEN category = 'womens-bags' THEN 'Bolsas Femininas'
+                WHEN category = 'womens-dresses' THEN 'Vestidos Femininos'
+                WHEN category = 'womens-jewellery' THEN 'Joias Femininas'
+                WHEN category = 'womens-shoes' THEN 'Calçados Femininos'
+                WHEN category = 'womens-watches' THEN 'Relógios Femininos'
+                ELSE category
+            END AS categoria,
             price_history.price AS preco
         FROM products
         JOIN price_history
